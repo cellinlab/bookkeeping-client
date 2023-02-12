0a7cd9a3d529305ca055dc311e88fec897dd65c6
cell <cellinlab@gmail.com>
2023-02-12 20:18:51 +0800

    feat init router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Home from '../container/Home'
+import About from '../container/About'
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+]
+
+export default routes
